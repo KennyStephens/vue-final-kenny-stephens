575a69ebbc5224b6c7a04627b9d0d8ae14022faa
Kenny Stephens <10468762@uvu.edu>
2018-10-23 12:15:11 -0600

    Structured app a bit.

--- src/router.js
+++ src/router.js
@@ -21,13 +21,13 @@ export default new Router({
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
-      path: "/layout",
-      name: "layout",
+      path: "/contact",
+      name: "contact",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Layout.vue")
+        import(/* webpackChunkName: "about" */ "./views/Contact.vue")
     }
   ]
 });
