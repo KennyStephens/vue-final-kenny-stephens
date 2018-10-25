aac07e0ae85a8e4e0b49b6855019923b789a19ad
Kenny Stephens <10468762@uvu.edu>
2018-10-25 13:04:33 -0600

    Added a bunch of new stuff.

---
+++ src/components/AppNewYouGen.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <button 
+            class="button is-block is-success mb-5 is-large ma-auto" 
+            @click="newYouGen">Generate New You!</button>
+            <div v-if="loadingAnimation">
+                <h1 class="title has-text-centered mb-0">GENERATING...</h1>
+                <img src="../assets/loader.gif" alt="Loading Animation" class="image ma-auto is-128x128">
+            </div>
+          <div 
+            v-for="you in newYouData" 
+            :key="you.name.first"
+            class="box has-text-centered">
+               <img 
+               :src="you.picture.large"
+               class="image is-rounded">
+               {{ you.gender }} - {{ you.name.first }} - {{ you.email }}
+                  
+                </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                newYouData: [],
+                loadingAnimation: false
+            }
+        },
+        methods: {
+            newYouGen() {
+                this.loadingAnimation = true;
+                setTimeout(() => {
+                    this.loadingAnimation = false;
+                    axios.get('https://randomuser.me/api/')
+                    .then(response => {
+                    this.newYouData = response.data.results;
+                    console.log(this.newYouData);
+                    })
+                    .catch(error => {
+                    console.log(error);
+                    });  
+                }, 3000);
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="hero">
+    <div class="hero is-fullheight">
         <div class="hero-body">
             <div class="container">
-                <h1 class="title is-1">hello</h1>
+                <h1 class="is-size-1 has-text-centered has-text-primary has-text-weight-semibold">Welcome to NewYou</h1>
+                <h1 class="subtitle has-text-centered">Are you sick of your current life? Want to start <span class="has-text-success is-size-4 has-text-weight-semibold">fresh</span>? Well you've come to the right place. Here at NewYou, the possibilities are endless and the wait time is short. Go ahead click the NewYou Generator and become the new you.</h1>
+
+                <app-new-you-gen></app-new-you-gen>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+    import AppNewYouGen from '../components/AppNewYouGen.vue'
 
+    export default {
+        components: {
+            AppNewYouGen
+        }
     }
 </script>
 
 <style scoped>
-
+    .is-fullheight {
+        background-image: url(../assets/main-background.jpg);
+        background-size: cover;
+    }
 </style>
