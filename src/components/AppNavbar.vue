e62c57489638da88e0a6e106305e2a2664678f09
Kenny Stephens <10468762@uvu.edu>
2018-10-25 13:10:47 -0600

    Added new navbar icon. Needs to be resized.

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    <a class="navbar-item" href="kennystephens.com">
+      <i class="fas fa-users fa-3x"></i>
     </a>
 
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
