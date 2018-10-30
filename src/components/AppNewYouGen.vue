b1461e6045773ab20ccbdba13a9215414f989541
Kenny Stephens <10468762@uvu.edu>
2018-10-30 08:46:31 -0600

    Messing around with cards

--- src/components/AppNewYouGen.vue
+++ src/components/AppNewYouGen.vue
@@ -1,77 +1,48 @@
 <template>
-    <div class="container">
-       
-                 <div class="columns">
-                <div class="column">
-                <div class="field results">
-                    <label class="label">Number of Results Requested</label>
-  <div class="control">
-    <input class="input is-primary" type="number" placeholder="Put Number Here">
-  </div>
-</div>
-            
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <div class="field results">
+          <label class="label">Number of Results Requested</label>
+            <div class="control">
+              <input class="input is-primary" type="number" placeholder="Put Number Here">
+            </div>
         </div>
-       </div>
-        <button 
-            class="button is-block is-success mb-5 is-large ma-auto" 
-            @click="newYouGen"
-            v-if="!loadingAnimation">Generate the NewYou!</button>
-            <transition name="fade">
-            <div v-if="loadingAnimation">
-                <h1 class="title has-text-centered mb-0">GENERATING...</h1>
-                <img 
-                    src="../assets/loader.gif" 
-                    alt="Loading Animation" 
-                    class="image ma-auto pa-2 mb-5 is-128x128">
-            </div>
-            </transition>
-            
-            <ul>
-                <transition-group name="list" tag="div">
-                <li  v-for="you in newYouData" 
-                    :key="you.name.first">
-                          <div class="columns box mb-5">
-                <div class="column img-column">
-                
-                <div>
-                        <figure class="image ma-auto">
-                            <img 
-                                class="is-rounded"
-                                :src="you.picture.large">
-                        </figure>
-                </div>
-                
-                    </div>
-                    <div class="column">
-                 
-                    <div 
-                    class="has-text-centered">
-                    <div class="box is-size-4">
-                        Your New First Name: <span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.name.first.toUpperCase() }}</span>
-                    </div>
-                    <div class="box is-size-4">
-                        Your New Last Name: <span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.name.last.toUpperCase() }}</span>
-                    </div>
-                    <div class="box is-size-4">
-                        Your New Age: <span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.dob.age }}</span>
-                    </div>
-                    <div class="box is-size-4">
-                        Your New Email: <span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.email }}</span>
-                    </div>
-                    <div class="box is-size-4">
-                        Your New Nationality : <span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.nat }}</span>
-                    </div>
-                </div>
-                
-                    </div>
-            </div>
-                </li>
-                </transition-group>
-            </ul>
-  
-
-
+      </div>
     </div>
+  <button 
+      class="button is-block is-success mb-5 is-large ma-auto" 
+      @click="newYouGen"
+      v-if="!loadingAnimation">Generate the NewYou!</button>
+      <transition name="fade">
+      <div v-if="loadingAnimation">
+          <h1 class="title has-text-centered mb-0">GENERATING...</h1>
+          <img 
+              src="../assets/loader.gif" 
+              alt="Loading Animation" 
+              class="image ma-auto pa-2 mb-5 is-128x128">
+      </div>
+      </transition>
+      
+      <ul>    
+        <div class="columns is-multiline">   
+          <li 
+            v-for="you in newYouData" 
+            :key="you.name.first"
+            class="column is-one-third box">
+              <figure class="image ma-auto">
+                <img 
+                    class="is-rounded "
+                    :src="you.picture.large">
+              </figure>
+              <p class="has-text-centered mt-2">Your New First Name: <br><span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.name.first.toUpperCase() }}</span></p>
+              <p class="has-text-centered">Your New Last Name: <br><span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.name.last.toUpperCase() }}</span></p>
+              <p class="has-text-centered">Your New Age: <br><span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.dob.age }}</span></p>
+              <p class="has-text-centered">Your New Email: <br><span class="has-text-primary has-text-weight-semibold is-size-4">{{ you.email }}</span></p>  
+          </li>
+        </div>
+      </ul>
+  </div>
 </template>
 
 <script>
@@ -137,14 +108,8 @@ export default {
   transform: translateX(-100px);
 }
 
-.img-column {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
 figure {
-  min-width: 200px;
+  max-width: 150px;
 }
 
 figure img {
@@ -156,5 +121,6 @@ figure img {
   flex-direction: column;
   align-items: center;
 }
+
 </style>
 
