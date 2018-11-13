36296149174648d2768e757e725e50a2ab7b557d
Kenny Stephens <10468762@uvu.edu>
2018-11-13 13:09:29 -0700

    fixed nav icon link

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,9 +1,11 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.kennystephens.com">
+      <router-link to="/">
+        <a class="navbar-item">
         <i class="fas fa-users fa-3x"></i>
       </a>
+      </router-link>
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
