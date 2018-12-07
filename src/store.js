efa53ee1a784aace261f1b0b14c046a05e0015d3
Kenny Stephens <10468762@uvu.edu>
2018-12-07 08:10:49 -0700

    Changed styling on the questions modal

--- src/store.js
+++ src/store.js
@@ -7,11 +7,10 @@ export default new Vuex.Store({
   state: {
     value: 0
   },
-  getters: {
+  mutations: {
     numberOfFavs: state => {
       const numberOfFavorites = JSON.parse(localStorage.getItem('newYouData'));
       state.value = numberOfFavorites.length;
-      return state.value
     }
   }
 });
